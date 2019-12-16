c50f1f93a4e23ccec2cb0232506c003a465d29f0
项硕 <m13661927770@163.com>
2019-12-16 23:05:49 +0800

    init

---
+++ src/main/java/org/catmint/server/io/FrontendAuthHandler.java
@@ -0,0 +1,23 @@
+package org.catmint.server.io;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 前台权限处理器
+ *
+ * @author Shuo Xiang
+ * @since 15 12月 2019
+ */
+@Slf4j
+public class FrontendAuthHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+    }
+}
